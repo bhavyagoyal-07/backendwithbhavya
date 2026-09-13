e6ec2a9079392b0725359ed3a202f4aba798620f
Bhavya Goyal <bhavyagoyal6294@gmail.com>
2026-09-13 19:29:03 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -13,6 +13,20 @@ dotenv.config({
 
 connectDB()
 
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port ${process.env.PORT}`)
+         app.on("errror",(error) => {
+            console.log("ERRR:",error);
+            throw error
+        })
+        
+    })
+})
+.catch((err) => {
+    console.log("M0NGODB CONNECTION FAILED!!!",err)
+})
+
 
 
 
